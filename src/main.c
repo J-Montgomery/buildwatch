f0ace645399780f0c54309108dcbaf10b2f5ac6c
Jordan Montgomery <montytyper@msn.com>
2021-08-22 12:53:43 -0700

    Began concatenating remaining argv input so that we can spawn a shell executing the command later.

--- src/main.c
+++ src/main.c
@@ -3,9 +3,14 @@
 #include <linux/ptrace.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #ifndef BUILDWATCH_NAME
 #define BUILDWATCH_NAME "buildwatch"
+#endif
+
+#ifndef DEFAULT_CMD_SIZE
+#define DEFAULT_CMD_SIZE (1024)
 #endif
 
 static struct option long_options[] = { { "verbose", no_argument, 0, 'V' },
@@ -28,8 +33,14 @@ int main(int argc, char **argv)
 {
 	int verbose = 0;
 	char *config_file = ".bwconf";
-	int index;
+	int idx;
 	int c;
+	size_t cmd_sz = 0;
+	size_t cmd_free_space = DEFAULT_CMD_SIZE;
+
+	char *command_str = calloc(DEFAULT_CMD_SIZE, 1);
+	if (command_str == NULL)
+		exit(EXIT_FAILURE);
 
 	opterr = 0;
 
@@ -69,9 +80,26 @@ int main(int argc, char **argv)
 		}
 	}
 
-	for (index = optind; index < argc; index++)
-		printf("Non-option argument %s\n", argv[index]);
+	for (idx = optind; idx < argc; idx++) {
+		size_t arg_sz = strlen(argv[idx]) + 1;
+		printf("Non-option argument %s\n", argv[idx]);
+		while (arg_sz >= cmd_free_space) {
+			command_str = realloc(command_str, DEFAULT_CMD_SIZE);
+			if (command_str == NULL)
+				exit(EXIT_FAILURE);
+			cmd_sz += DEFAULT_CMD_SIZE;
+			cmd_free_space += DEFAULT_CMD_SIZE;
+		}
 
+		memcpy(command_str + cmd_sz, *(argv + idx), arg_sz);
+
+		cmd_free_space -= arg_sz;
+		cmd_sz += arg_sz;
+		command_str[cmd_sz - 1] = ' ';
+	}
+
+	printf("len: %zi counted: %zi remaining: %zi\n%s\n",
+	       strlen(command_str), cmd_sz, cmd_free_space, command_str);
 	printf("Hello, World!\n");
 	return 0;
 }
